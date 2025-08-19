42fa00a041dff44b57ed83aa1212671ac02dadca
TheOutcastVirus <vikram.kommera@gmail.com>
2025-08-18 20:50:28 -0700

    assignment complete!

--- src/your_solution/src/spin_slow_update.cpp
+++ src/your_solution/src/spin_slow_update.cpp
@@ -8,8 +8,47 @@ int main(int argc, char *argv[]) {
 }
 
 SlowSolution::SlowSolution() : Node("slowsolution") {
-  RCLCPP_INFO(this->get_logger(), "Remove this statement from spin_slow_update.cpp");
+  // RCLCPP_INFO(this->get_logger(), "Remove this statement from spin_slow_update.cpp");
   // your code here
+  measuredPosSubscriber = this->create_subscription<ArrayMsg>("measuredpos", 10, std::bind(&SlowSolution::measuredPosCallback, this, std::placeholders::_1));
+  measuredVelSubscriber = this->create_subscription<ArrayMsg>("measuredvel", 10, std::bind(&SlowSolution::measuredVelCallback, this, std::placeholders::_1));
+
+  predPosPublisher = this->create_publisher<ArrayMsg>("predictedpos", 10);
+  timer = this->create_wall_timer(std::chrono::milliseconds(1), std::bind(&SlowSolution::timerCallback, this));
+  
+  pos_vec = {0.0, 0.0};
+  vel_vec = {0.0, 0.0};
+  pred_pos_vec = {0.0, 0.0};
+  
+  // So this doesnt crash if we dont have data yet
+  pos_received = false;
+  vel_received = false;
+  
+  last_vel_time = std::chrono::steady_clock::now();
 }
 
-// your code here
+void SlowSolution::measuredPosCallback(const ArrayMsg::SharedPtr msg) {
+  pos_vec = msg->data;
+  pos_received = true;
+}
+
+void SlowSolution::measuredVelCallback(const ArrayMsg::SharedPtr msg) {
+  vel_vec = msg->data;
+  last_vel_time = std::chrono::steady_clock::now();
+  vel_received = true;
+}
+
+void SlowSolution::timerCallback() {
+  if (!pos_received || !vel_received) {
+    return;
+  }
+  
+  std::chrono::steady_clock::time_point current_time = std::chrono::steady_clock::now();
+  double dt = std::chrono::duration<double>(current_time - last_vel_time).count();
+  
+  pred_pos_vec = {pos_vec[0] + vel_vec[0] * dt, pos_vec[1] + vel_vec[1] * dt};
+  
+  std_msgs::msg::Float64MultiArray msg;
+  msg.data = pred_pos_vec;
+  predPosPublisher->publish(msg);
+}
--- src/your_solution/src/spin_slow_update.h
+++ src/your_solution/src/spin_slow_update.h
@@ -3,13 +3,39 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/float64_multi_array.hpp"
+#include <vector>
+#include <chrono>
+#include <functional>
+
 using ArrayMsg = std_msgs::msg::Float64MultiArray;
 
 class SlowSolution : public rclcpp::Node {
  public:
   SlowSolution();
  private:
-  // your code here
+  rclcpp::Subscription<ArrayMsg>::SharedPtr measuredPosSubscriber;
+  rclcpp::Subscription<ArrayMsg>::SharedPtr measuredVelSubscriber;
+
+  void measuredPosCallback(const ArrayMsg::SharedPtr msg);
+  void measuredVelCallback(const ArrayMsg::SharedPtr msg);
+
+  std::vector<double> predictPos();
+
+  std::vector<double> pos_vec;
+  rclcpp::Time pos_time;
+  std::vector<double> vel_vec;
+  rclcpp::Time vel_time;
+
+  std::vector<double> pred_pos_vec;
+  
+  bool pos_received;
+  bool vel_received;
+  
+  std::chrono::steady_clock::time_point last_vel_time;
+
+  rclcpp::TimerBase::SharedPtr timer;
+  rclcpp::Publisher<ArrayMsg>::SharedPtr predPosPublisher;
+  void timerCallback();
 };
 
 #endif //YOUR_SOLUTION_SRC_SPIN_SLOW_UPDATE_H_
